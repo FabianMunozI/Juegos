b5bce61c893b39002abe3d76b97983f5a646378d
fcopizarro <francisco.pizarroj@usm.cl>
2023-06-20 19:12:07 -0400

    ingore

--- Assets/Scripts/Quests/Balde_Comp.cs
+++ Assets/Scripts/Quests/Balde_Comp.cs
@@ -7,9 +7,21 @@ public class Balde_Comp : Interactable
     public bool tiene_agua = false;
     private Mesh bal_con_agua;
     private Mesh bal_sin_agua;
+    private GameObject water_mesh;
 
     void Start()
     {
         tiene_agua = false;
+        water_mesh = GameObject.Find("water 2");
+    }
+
+    void Update()
+    {
+        if (tiene_agua)
+        {
+            Debug.Log("aa");
+        }else{
+            water_mesh.active = false;  
+        }
     }
 }
