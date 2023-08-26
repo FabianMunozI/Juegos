9cbee2f0599054db4289dc48b61d0bcd363ed8e6
FabianMunozI <fabian.munozi@usm.cl>
2023-08-26 01:21:25 -0400

    NavMesh adaptable

---
+++ Assets/AlinearPlanoNavMesh.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AlinearPlanoNavMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 posPlayer;
+    void Start()
+    {
+        posPlayer = GameObject.Find("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(posPlayer.x, 100f, posPlayer.z);
+    }
+}
