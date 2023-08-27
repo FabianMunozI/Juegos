d8e44bdddbf8ae183f16d27090ec3ed023f022b6
FabianMunozI <fabian.munozi@usm.cl>
2023-08-26 21:05:17 -0400

    Animaciones Responsivas

--- Assets/Scripts/Quests/activarMisionRitmo.cs
+++ Assets/Scripts/Quests/activarMisionRitmo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,13 @@ public class activarMisionRitmo : Interactable
     GameObject refPosCamAlcalde;
 
     GameObject microfonoRef;
+    bool activarUpdate;
+    float distanceCamPos;
+
+    Vector3 initialDistance;
+
+    bool segUpdate;
+
     void Start()
     {
         CScenarioRef = GameObject.Find("CScenario");
@@ -24,6 +32,37 @@ public class activarMisionRitmo : Interactable
 
         player = GameObject.Find("Player");
         microfonoRef = GameObject.Find("Podio").transform.GetChild(0).gameObject;
+    }
+
+    void Update(){
+        if(activarUpdate){
+            player.transform.position += initialDistance * 1/distanceCamPos * Time.deltaTime * 4;
+
+            Vector3 direct = (refAlcalde.transform.position - player.transform.position).normalized;
+            Quaternion rotGoal = Quaternion.LookRotation(direct);
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, rotGoal, 20f *Time.deltaTime);
+
+            if(Vector3.Distance(player.transform.position, refPosCamAlcalde.transform.position) <=0.5){
+                activarUpdate=false;
+                Invoke("activarInvoke2daAnimacion",1f);
+                distanceCamPos = (Vector3.Distance(player.transform.position, microfonoRef.transform.GetChild(3).transform.position)) /* / 4f */; // distancia dividido tiempo
+                initialDistance = microfonoRef.transform.GetChild(3).transform.position - player.transform.position;
+            } 
+        }
+
+        if(segUpdate){
+            player.transform.position += initialDistance * 1/distanceCamPos * Time.deltaTime * 4;
+
+            Vector3 direct = (microfonoRef.transform.parent.GetChild(2).transform.position - player.transform.position).normalized;
+            Quaternion rotGoal = Quaternion.LookRotation(direct);
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, rotGoal, 2f *Time.deltaTime);
+
+            if(Vector3.Distance(player.transform.position, microfonoRef.transform.GetChild(3).transform.position) <=0.5){
+                segUpdate = false;
+                player.GetComponent<Collider>().isTrigger = false;
+                player.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
     }
 
     public override void Interact(){ // preguntar mision
@@ -38,20 +77,22 @@ public class activarMisionRitmo : Interactable
         // ACTIVAR POLITICO, Y NPCS
         CScenarioRef.transform.GetChild(0).gameObject.SetActive(true);
 
-        ///////// paneo de camara
+        ///////// paneo de camara suave
+        
+        player.GetComponent<Rigidbody>().isKinematic = true;
+        player.GetComponent<Collider>().isTrigger = true;
+        //player.transform.position = refPosCamAlcalde.transform.position;
         
 
+        distanceCamPos = (Vector3.Distance(player.transform.position, refPosCamAlcalde.transform.position)) /* / 4f */; // distancia dividido tiempo
+        initialDistance = refPosCamAlcalde.transform.position - player.transform.position;
+        activarUpdate = true;
 
         //////////// Activar tablero Ritmo
         MisionRitmo.transform.GetChild(0).gameObject.SetActive(true);
 
         //////////////////////////////  Mover player posicion y rotacion camara a escenario.
 
-        player.transform.position = microfonoRef.transform.GetChild(3).transform.position;//new Vector3(-45, 2.13f, -94.75f);
-
-        Vector3 targetPosition = microfonoRef.transform.position;
-        targetPosition.y = player.transform.position.y;
-        player.transform.LookAt(targetPosition);
     
         ////////////////////////// desactiva el canvas si iniciar o no misin
 
@@ -64,6 +105,10 @@ public class activarMisionRitmo : Interactable
         MisionRitmo.transform.GetChild(1).gameObject.SetActive(false);
         SetActivePlayerScripts(true);
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void activarInvoke2daAnimacion(){
+        segUpdate = true;
     }
 
     void SetActivePlayerScripts(bool valor){
