8a7cd19aa7d5f4bad5e8b4a0b538fe44d9229691
Godiessssseh <diego.rosalesl@sansano.usm.cl>
2023-08-29 23:03:04 -0400

    AgregarMenu

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void MenuButton ()
+    {
+        SceneManager.LoadScene("MenuGenerador");
+    }
+
+    // Update is called once per frame
+    public void OnQuitButton ()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackButton ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void GameStart()
+    {
+        //Decidir que escena vamos a cargar lol
+        SceneManager.LoadScene("Bosque");
+    }
+    public void GameStart2()
+    {
+        //Decidir que escena vamos a cargar lol
+        SceneManager.LoadScene("Ciudad");
+    }
+
+}
