ceb6279a4a79ede3696f4ea819af2a630ba214be
FabianMunozI <fabian.munozi@usm.cl>
2023-08-31 00:49:03 -0400

    buildeo error fixeo

--- Assets/Scripts/Quests/RitmoTeclas.cs
+++ Assets/Scripts/Quests/RitmoTeclas.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using GLTF.Schema;
+//using GLTF.Schema;
 using JetBrains.Annotations;
 using TMPro;
-using UnityEditorInternal;
+//using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
